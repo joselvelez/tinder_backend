5cc9668df4ebdd4554d12a6ae01658e5c0772d7d
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2021-09-30 08:41:54 -0500

    1:42 .env issue fixed

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import Express from 'express';
 import Mongoose from 'mongoose';
-import './loadEnv';
 
 // App Config
 const app = Express();
 const PORT = process.env.PORT || 8001;
-const connection_url = 'mongodb+srv://tinder-clone:@cluster0.0vqlh.mongodb.net/tinder-clone?retryWrites=true&w=majority'
+const connection_url = `mongodb+srv://tinder-clone:${process.env.DBPASS}@cluster0.0vqlh.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
 
 // Middlewares
 
