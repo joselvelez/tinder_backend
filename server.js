f79e01d9d804d9cab908711bce8fff0022f36e13
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2021-09-30 20:53:44 -0500

    2:00 talkign to mongodb via endpoints

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Express from 'express';
 import Mongoose from 'mongoose';
+import cors from 'cors';
+import Cards from './dbCards.js';
 
 // App Config
 const app = Express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 8001;
 const connection_url = `mongodb+srv://tinder-clone:${process.env.DBPASS}@cluster0.0vqlh.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
 
 // Middlewares
+app.use(cors());
+app.use(Express.json());
 
 // DB Config
 Mongoose.connect(connection_url);
@@ -17,6 +21,28 @@ Mongoose.connect(connection_url);
 app.get('/', (req, res) => {
     return res.status(200)
         .send('Hellooooo')
+});
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
 });
 
 // Listener
